d5cf150396a6e7874cac87c9402bd0234478b8a0
Joshua Kim <bki42@uclive.ac.nz>
2024-05-15 21:51:52 +1200

    update for petitions

--- assignment/src/components/Petitions.tsx
+++ assignment/src/components/Petitions.tsx
@@ -1,35 +1,71 @@
 import axios from 'axios';
-import React from "react";
+import React, {ChangeEvent} from "react";
 import {Link} from 'react-router-dom';
 import Petition from './Petition';
-
+import {DataGrid, GridCellParams, GridColDef} from '@mui/x-data-grid';
+import Container from '@mui/material/Container';
+import {Button, Chip, TextField} from "@mui/material";
 
 
 const Petitions = () => {
     const [petitions, setPetitions] = React.useState<Array<Petition>>([])
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState("")
+    const [query, setQuery] = React.useState("")
+
+
     React.useEffect(() => {
         getPetitions()
     }, [])
+
     const getPetitions = () => {
         axios.get('http://localhost:4941/api/v1/petitions')
             .then((response) => {
                 setErrorFlag(false)
                 setErrorMessage("")
                 setPetitions(response.data.petitions)
+
             }, (error) => {
                 setErrorFlag(true)
                 setErrorMessage(error.toString())
             })
     }
+    const getQueryPetitions = () => {
+        axios.get('http://localhost:4941/api/v1/petitions?q=' + query)
+            .then((response) => {
+                setErrorFlag(false)
+                setErrorMessage("")
+                setPetitions(response.data.petitions)
+                // setSerchedNumber(response.data.count)
+            }, (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.toString())
+            })
+    }
+
+    const searchPetitionState = (event:ChangeEvent<HTMLInputElement>) => {
+        setQuery(event.target.value)
+    }
+    const handleSearchClick = () => {
+        if ( query.length === 0) {
+            getPetitions()
+        } else {
+        getQueryPetitions()
+            }
+    }
+    const handleKeyPress = (event: any) => {
+        if (event.key === 'Enter') {
+            handleSearchClick();
+        }
+    };
+
+
     const list_of_petitions = () => {
         return petitions.map((petition: Petition) =>
             <tr key={petition.petitionId}>
                 <th scope="row">{petition.petitionId}</th>
                 <td>{petition.title}</td>
-                {/*<td><Link to={"/users/" + petition.petitionId}>Go to*/}
-                {/*    petition</Link></td>*/}
+
                 <td>
                     <button type="button">Delete</button>
                     <button type="button">Edit</button>
@@ -37,6 +73,21 @@ const Petitions = () => {
             </tr>
         )
     }
+    const columns: GridColDef[] = [
+        { field: 'image', headerName: 'Image', width: 130, renderCell: (params: GridCellParams) => {
+                const petitionId = params.row.petitionId as number;
+                return <img src={'http://localhost:4941/api/v1/petitions/' + petitionId + "/image"} alt="Petition Image" />;
+            }},
+        { field: 'petitionId', headerName: 'ID', width: 70},
+        { field: 'title', headerName: 'Title', width: 130, align: 'center' },
+        { field: 'ownerFirstName', headerName: 'Owner First Name', width: 130, align: 'center' },
+        { field: 'ownerLastName', headerName: 'Owner Last Name', width: 130, align: 'center' },
+        { field: 'numberOfSupporters', headerName: 'No. of Supporters', width: 130, align: 'center' },
+        { field: 'creationDate', headerName: 'Creation Date', width: 200, align: 'center' },
+
+    ];
+    const getRowId = (row:Petition) => row.petitionId;
+
 
     if (errorFlag) {
         return (
@@ -52,18 +103,29 @@ const Petitions = () => {
             return (
                 <div>
                     <h1>Petitions</h1>
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">title</th>
-                            <th scope="col">description</th>
-                            <th scope="col">actions</th>
-                        </tr>
-                        </thead>
-                        <tbody> {list_of_petitions()}
-                        </tbody>
-                    </table>
+                        <Container maxWidth="xl">
+                            <TextField style={{height: 25, width: '80%'}}
+                                       id="outlined-basic" label="Search Petition" variant="outlined"
+                                        value={query} onChange={searchPetitionState} onKeyPress={handleKeyPress}/>
+                            <Button style={{height: 55, width: '20%', fontSize: '1.5rem'}} variant="contained"
+                                    onClick={handleSearchClick}>Search</Button>
+                            <div style={{marginTop: '50px', height: 400, width: '100%'}}>
+                                <DataGrid
+                                    rows={petitions}
+                                    getRowId={getRowId}
+                                    rowHeight={50}
+                                    columns={columns}
+                                    initialState={{
+                                        pagination: {
+                                            paginationModel: {page: 0, pageSize: 5},
+                                        },
+                                    }}
+                                    pageSizeOptions={[5, 10]}
+                                    checkboxSelection
+
+                                />
+                            </div>
+                        </Container>
                 </div>
             )
         }
---
+++ assignment/src/types/user.d.ts
@@ -0,0 +1,11 @@
+type User = {
+    id: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    imageFilename: string,
+    authToken: string,
+
+}
+
