ce6461525849d499718f2508d9140aa1d280ce7e
Joshua Kim <bki42@uclive.ac.nz>
2024-05-15 23:52:46 +1200

    update for petitions

--- assignment/src/components/Petitions.tsx
+++ assignment/src/components/Petitions.tsx
@@ -2,9 +2,12 @@ import axios from 'axios';
 import React, {ChangeEvent} from "react";
 import {Link} from 'react-router-dom';
 import Petition from './Petition';
-import {DataGrid, GridCellParams, GridColDef} from '@mui/x-data-grid';
+import {DataGrid, GridCellParams, GridColDef, GridRowParams} from '@mui/x-data-grid';
 import Container from '@mui/material/Container';
 import {Button, Chip, TextField} from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+
+import {format} from "node:url";
 
 
 const Petitions = () => {
@@ -59,7 +62,6 @@ const Petitions = () => {
         }
     };
 
-
     const list_of_petitions = () => {
         return petitions.map((petition: Petition) =>
             <tr key={petition.petitionId}>
@@ -74,16 +76,27 @@ const Petitions = () => {
         )
     }
     const columns: GridColDef[] = [
-        { field: 'image', headerName: 'Image', width: 130, renderCell: (params: GridCellParams) => {
-                const petitionId = params.row.petitionId as number;
-                return <img src={'http://localhost:4941/api/v1/petitions/' + petitionId + "/image"} alt="Petition Image" />;
+        { field: 'petitionImage', headerName: 'Petition Image', headerAlign: 'center', width: 130,
+            align: 'center', renderCell: (aparams: GridCellParams) => {
+                const petitionId = aparams.row.petitionId as number;
+                return <img src={'http://localhost:4941/api/v1/petitions/' + petitionId + "/image"}
+                            alt="Petition Image"
+                            style={{ maxWidth: '100%', height: 'auto' }}
+                />;
             }},
-        { field: 'petitionId', headerName: 'ID', width: 70},
-        { field: 'title', headerName: 'Title', width: 130, align: 'center' },
-        { field: 'ownerFirstName', headerName: 'Owner First Name', width: 130, align: 'center' },
-        { field: 'ownerLastName', headerName: 'Owner Last Name', width: 130, align: 'center' },
-        { field: 'numberOfSupporters', headerName: 'No. of Supporters', width: 130, align: 'center' },
-        { field: 'creationDate', headerName: 'Creation Date', width: 200, align: 'center' },
+        { field: 'title', headerName: 'Title',headerAlign: 'center', width: 300, align: 'center' },
+        { field: 'ownerFirstName', headerName: 'Owner First Name',headerAlign: 'center', width: 130, align: 'center' },
+        { field: 'ownerLastName', headerName: 'Owner Last Name',headerAlign: 'center', width: 130, align: 'center' },
+        { field: 'userImage', headerName: 'Owner Image',headerAlign: 'center', width: 130,
+            align: 'center', renderCell: (params: GridCellParams) => {
+                const userId = params.row.ownerId as number;
+                return <Avatar style={{ marginTop: 4, width: 100, height: 100, borderRadius: '50%', overflow: 'hidden', justifyContent: "center", alignContent:"center"}}
+                    src={'http://localhost:4941/api/v1/users/' + userId + "/image"} />
+
+            }},
+        { field: 'numberOfSupporters', headerName: 'No. of Supporters',headerAlign: 'center', width: 130,
+            align: 'center' },
+        { field: 'creationDate', headerName: 'Creation Date',headerAlign: 'center', width: 200, align: 'center'},
 
     ];
     const getRowId = (row:Petition) => row.petitionId;
@@ -103,17 +116,18 @@ const Petitions = () => {
             return (
                 <div>
                     <h1>Petitions</h1>
-                        <Container maxWidth="xl">
+                        <Container style={{ height: 800}}>
                             <TextField style={{height: 25, width: '80%'}}
                                        id="outlined-basic" label="Search Petition" variant="outlined"
                                         value={query} onChange={searchPetitionState} onKeyPress={handleKeyPress}/>
                             <Button style={{height: 55, width: '20%', fontSize: '1.5rem'}} variant="contained"
                                     onClick={handleSearchClick}>Search</Button>
-                            <div style={{marginTop: '50px', height: 400, width: '100%'}}>
+                            <div style={{marginTop: '50px', height: 715, width: 1300} }>
                                 <DataGrid
                                     rows={petitions}
                                     getRowId={getRowId}
-                                    rowHeight={50}
+                                    rowHeight={110}
+                                    style={{alignContent:"center", justifyContent: "center",}}
                                     columns={columns}
                                     initialState={{
                                         pagination: {
@@ -121,7 +135,7 @@ const Petitions = () => {
                                         },
                                     }}
                                     pageSizeOptions={[5, 10]}
-                                    checkboxSelection
+                                    // checkboxSelection
 
                                 />
                             </div>
